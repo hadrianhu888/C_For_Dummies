b4cee1d37576c854fa7ca5d3076f4bc1cabdf968
hadrianhu888 <hadrian.hu@gmail.com>
2023-01-27 15:30:26 -0500

    changes to MAKEFILE s

---
+++ ex1p4/ex1p4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define SPACE printf("\n\n");
+#define MAX 300
+
+int main(int argc, char ** argv)
+{
+    int i;
+    for (i = 0; i < MAX; i++)
+    {
+        SPACE;
+        printf("Hello World");
+    }
+    return 0;
+}
---
+++ ex3/ex1p3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define SPACE printf("\n\n");
+int main(int argc, char ** argv)
+{
+    SPACE;
+    printf("Hello World");
+    SPACE; 
+    printf("Greetings from the other side");
+    SPACE; 
+    return 0;
+}
